e8bc701386045f9d6c26887a1ba375416a9e02d8
vb0004 <vebrie91@gmail.com>
2022-08-19 12:33:29 +0700

    a

---
+++ src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Counter</h1>
+    <p>{{ count }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CounterView',
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+  },
+};
+</script>
+
+<style></style>
