7f2c29a70283ae13eb94015fd8be555c36d92d8c
vb0004 <vebrie91@gmail.com>
2022-08-21 00:46:38 +0700

    a

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,20 @@
   </nav>
   <router-view/> -->
   <div>
+    <NavBar />
     <h1 class="text-center m-4">My App</h1>
     <router-view></router-view>
   </div>
 </template>
-
+<script>
+import NavBar from './components/NavBar.vue';
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  },
+};
+</script>
 <style scoped>
 @import 'style.css';
 </style>
