4782eaff1d1ccbfee47e6e8baaf5812fe45e98bd
vb0004 <vebrie91@gmail.com>
2022-08-21 03:41:28 +0700

    a

--- src/main.js
+++ src/main.js
@@ -3,5 +3,14 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-createApp(App).use(store).use(router).mount('#app');
+// import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './assets/dist/css/tabler.min.css';
+import './assets/dist/css/tabler-flags.min.css';
+import './assets/dist/css/tabler-payments.min.css';
+import './assets/dist/css/tabler-vendors.min.css';
+import './assets/dist/css/demo.min.css';
+
+const apps = createApp(App);
+apps.use(store);
+apps.use(router);
+apps.mount('#app');
